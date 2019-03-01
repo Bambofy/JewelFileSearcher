44b46de3626a9f18df21ded107d1014ca571363b
Richard Bamford <rbamfordz@gmail.com>
2019-03-01 21:17:12 +0000

    version 1

---
+++ JewelListGenerator/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace JewelListGenerator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRunJewel = new System.Windows.Forms.Button();
+            this.txtBoxDirectoryInput = new System.Windows.Forms.TextBox();
+            this.txtBoxSearchString = new System.Windows.Forms.TextBox();
+            this.checkBoxListJewels = new System.Windows.Forms.CheckBox();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.FileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FileLineNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnExport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRunJewel
+            // 
+            this.btnRunJewel.Location = new System.Drawing.Point(13, 76);
+            this.btnRunJewel.Name = "btnRunJewel";
+            this.btnRunJewel.Size = new System.Drawing.Size(91, 35);
+            this.btnRunJewel.TabIndex = 1;
+            this.btnRunJewel.Text = "Run";
+            this.btnRunJewel.UseVisualStyleBackColor = true;
+            this.btnRunJewel.Click += new System.EventHandler(this.btnRunJewel_Click);
+            // 
+            // txtBoxDirectoryInput
+            // 
+            this.txtBoxDirectoryInput.Location = new System.Drawing.Point(13, 12);
+            this.txtBoxDirectoryInput.Name = "txtBoxDirectoryInput";
+            this.txtBoxDirectoryInput.Size = new System.Drawing.Size(249, 26);
+            this.txtBoxDirectoryInput.TabIndex = 2;
+            this.txtBoxDirectoryInput.Text = "Root directory";
+            // 
+            // txtBoxSearchString
+            // 
+            this.txtBoxSearchString.Location = new System.Drawing.Point(13, 44);
+            this.txtBoxSearchString.Name = "txtBoxSearchString";
+            this.txtBoxSearchString.Size = new System.Drawing.Size(249, 26);
+            this.txtBoxSearchString.TabIndex = 3;
+            this.txtBoxSearchString.Text = "String to match";
+            // 
+            // checkBoxListJewels
+            // 
+            this.checkBoxListJewels.AutoSize = true;
+            this.checkBoxListJewels.Location = new System.Drawing.Point(110, 82);
+            this.checkBoxListJewels.Name = "checkBoxListJewels";
+            this.checkBoxListJewels.Size = new System.Drawing.Size(121, 24);
+            this.checkBoxListJewels.TabIndex = 4;
+            this.checkBoxListJewels.Text = "List Jewels?";
+            this.checkBoxListJewels.UseVisualStyleBackColor = true;
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToOrderColumns = true;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FileName,
+            this.FileLineNumber});
+            this.dataGridView.Location = new System.Drawing.Point(13, 117);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 28;
+            this.dataGridView.Size = new System.Drawing.Size(754, 514);
+            this.dataGridView.TabIndex = 5;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // FileName
+            // 
+            this.FileName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.FileName.HeaderText = "File Name";
+            this.FileName.Name = "FileName";
+            // 
+            // FileLineNumber
+            // 
+            this.FileLineNumber.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.FileLineNumber.HeaderText = "Line Number";
+            this.FileLineNumber.Name = "FileLineNumber";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(611, 72);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(141, 34);
+            this.btnExport.TabIndex = 6;
+            this.btnExport.Text = "Export .CSV";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 653);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.checkBoxListJewels);
+            this.Controls.Add(this.txtBoxSearchString);
+            this.Controls.Add(this.txtBoxDirectoryInput);
+            this.Controls.Add(this.btnRunJewel);
+            this.Name = "MainForm";
+            this.Text = "Jewel";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRunJewel;
+        private System.Windows.Forms.TextBox txtBoxDirectoryInput;
+        private System.Windows.Forms.TextBox txtBoxSearchString;
+        private System.Windows.Forms.CheckBox checkBoxListJewels;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileLineNumber;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ JewelListGenerator/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JewelListGenerator
+{
+    public partial class MainForm : Form
+    {
+        List<JewelLineItem> _foundLines = new List<JewelLineItem>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private string GetFileNameFromPath(string pPath)
+        {
+            string[] parts = pPath.Split('/');
+            return parts[parts.Length - 1];
+        }
+
+        private void btnRunJewel_Click(object sender, EventArgs e)
+        {
+            _foundLines = new List<JewelLineItem>();
+
+            // get directory path.
+            string dirPath = txtBoxDirectoryInput.Text;
+
+            // get string to search.
+            string searchString = txtBoxSearchString.Text;
+
+            // do we auto list [JEWEL]
+            bool shouldListJewels = checkBoxListJewels.Checked;
+
+            // recurse through the root directory.
+            // for each file that is not a directory.
+            //  for each line in the file:
+            //      - keep track of the line number.
+            //      - test whether the line contains the string we are looking for.
+            //      - if yes the append the line infomation to a list.
+
+            string[] filePathList = Directory.GetFiles(dirPath, "*.*", SearchOption.AllDirectories);
+
+            foreach (string filePath in filePathList)
+            {
+                StreamReader fs = new StreamReader(filePath);
+
+                int lineNumber = 0;
+                while (!fs.EndOfStream)
+                {
+                    lineNumber++;
+
+                    string line = fs.ReadLine();
+
+                    bool containsSearchString = line.Contains(searchString);
+                    bool containsJewelString = line.Contains("[JEWEL]");
+
+                    if (containsSearchString || containsJewelString)
+                    {
+                        JewelLineItem item = new JewelLineItem();
+                        item.SourceFile = GetFileNameFromPath(filePath);
+                        item.LineNumber = lineNumber;
+
+                        _foundLines.Add(item);
+                    }
+                }
+
+                fs.Close();
+            }
+
+            dataGridView.Rows.Clear();
+
+            foreach (JewelLineItem lineItem in _foundLines)
+            {
+                dataGridView.Rows.Add(lineItem.SourceFile, lineItem.LineNumber);
+            }
+        }
+        
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "CSV file (*.csv)|*.csv| All Files (*.*)|*.*";
+            saveFileDialog1.Title = "Save an Comma Seperated Values file.";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.  
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.  
+                System.IO.FileStream fs =
+                   (System.IO.FileStream)saveFileDialog1.OpenFile();
+
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine("Filename,LineNumber");
+                foreach (JewelLineItem item in _foundLines)
+                {
+                    sw.WriteLine(item.SourceFile + "," + item.LineNumber);
+                }
+                sw.Close();
+
+                fs.Close();
+            }
+        }
+    }
+
+    public class JewelLineItem
+    {
+        public string SourceFile = "N/A";
+        public int LineNumber = -1;
+    }
+}
